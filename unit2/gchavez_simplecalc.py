f004b8ce8e9f86d3a820ca179baeb4ea5173373e
Gonzalo Chavez <gonzalochavez@uagateway.org>
2016-10-05 16:17:09 +0000

    Figuring out how to complete unit 2 lab 1

---
+++ unit2/gchavez_simplecalc.py
@@ -0,0 +1,6 @@
+first_digit = input("Enter a number ")
+oper = input("Enter a operation ")
+second_digit = input("Enter a second number ")
+
+solu = int(first_digit) + int(oper) + int(second_digit)
+print("The result is {}".format(soul))
