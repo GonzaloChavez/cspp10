1b1c4e7f745776380c757e28644df644ba15cf61
Gonzalo Chavez <gonzalochavez@uagateway.org>
2016-10-13 16:17:05 +0000

    Finished Lab 2

--- unit2/GChavez_pluralizer.py
+++ unit2/GChavez_pluralizer.py
@@ -1,22 +1,26 @@
-num = input("Enter a number ")
-noun = input("Enter a noun ")
+num = input("Enter a number: ")
+noun = input("Enter a noun: ")
 
-if int(num) == 0 and int(num) > 1 or noun[-2:] == "fe":
-    print(str(num) + str(noun[:-2]) + "ves")
-elif int(num) == 1:
-    print(str(num) + noun)
+if num == "1":
+    print("1 " + noun)
+else:
+    if noun[-2:] == "fe":
+        print(num + " " + noun[:-2] + "ves") 
+        
+    elif noun[-2:] == "ay" or noun[-2:] == "uy" or noun[-2:] == "oy" or noun[-2:] == "ey":
+        print(num + " " + noun + "s")
+
+    elif noun[-1] == "y":
+        print(num + " " + noun[:-1] + "ies")
+ 
+    elif noun[-2:] == "sh" or noun[-2:] ==  "ch":
+        print(num + " " + noun + "es")
+
+    elif noun[-2:] == "us":
+        print(num + " " + noun[:-2] + "i")
+    else: 
+        print (num + " " + noun + "s")
+
+
     
-elif int(num) == 0 and int(num) > 1 or noun[-1:] == "y":
-    print(str(num) + str(noun[:-1]) + "ies")
-elif int(num) == 1:
-    print(str(num) + noun)
     
-elif int(num) == 0 and int(num) > 1 or noun[-2:] == "sh" or "ch":
-    print(str(num) + str(noun) + "es")
-elif int(num) == 1:
-    print(str(num) + noun)
-    
-elif int(num) == 0 and int(num) > 1 or noun[-2:] == "us":
-    print(str(num) + str(noun[:-2]) + "i")
-elif int(num) == 1:
-    print(str(num) + noun)
