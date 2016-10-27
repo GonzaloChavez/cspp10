92df59849577719623d27122583130cab8253297
Gonzalo Chavez <gonzalochavez@uagateway.org>
2016-10-27 16:02:39 +0000

    Finished lab 3

--- unit3/GChavez_fizzbuzz.py
+++ unit3/GChavez_fizzbuzz.py
@@ -1,13 +1,15 @@
-num = input("Enter a number: ")
-new_num = int(num)
+num = int(input("Enter a number: "))
 
-for x in range(1,new_num + 1):
-    if new_num % 3 == 0:
-        break
-    print("Fizz")
-  
-    elif new num_num % 5 == 0:
-        break
-    print("Buzz")
-        
-print(new_num)
+
+for x in range(1,num + 1):
+    if x % 3 == 0 and x % 5 == 0:
+        print("FizzBuzz")
+    
+    elif x % 3 == 0:
+        print("Fizz")
+     
+    elif x % 5 == 0:
+        print("Buzz")
+
+    else:
+        print(x)
