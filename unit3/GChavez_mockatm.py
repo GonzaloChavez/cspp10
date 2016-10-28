43c30ca613202ec910fd15b88587cd6256ebfd32
Gonzalo Chavez <gonzalochavez@uagateway.org>
2016-10-28 16:16:09 +0000

    Finishing up pre lab 3-2

---
+++ unit3/GChavez_mockatm.py
@@ -0,0 +1,16 @@
+opt = input("Would you rather Withdraw, Deposit, or Exit: ")
+money = 10000
+
+while money > 0:
+        if opt == 'Withdraw':
+            w = input("How much will you like to take out: ")
+            break
+        
+            w > 10000
+            print("You don't have that kind of money")
+            break
+        
+            money = money - w
+            print(money)
+    
+        
