200160d6437ddb0799b1c024ddf3e48b8bc890be
Gonzalo Chavez <gonzalochavez@uagateway.org>
2016-11-01 15:52:45 +0000

    Finished up Pre-Lab 3-2

--- unit3/GChavez_mockatm.py
+++ unit3/GChavez_mockatm.py
@@ -2,15 +2,21 @@ opt = input("Would you rather Withdraw, Deposit, or Exit: ")
 money = 10000
 
 while money > 0:
-        if opt == 'Withdraw':
-            w = input("How much will you like to take out: ")
-            break
+        if opt == ("Withdraw"):
+            w = int(input("How much do you want to take out: "))
+            if (w > money):
+                print("You don't have that kind of money")
+            elif (w <= money):
+                money = money - w
+                print(money)
+                break
         
-            w > 10000
-            print("You don't have that kind of money")
-            break
-        
-            money = money - w
+        elif opt == ("Deposit"):
+            d = int(input("How much do you want to put in: "))
+            money = money + d
             print(money)
-    
+            break
+        
+        elif opt == ("Exit"):
+            break
         
