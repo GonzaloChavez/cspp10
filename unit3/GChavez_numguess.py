ac9387b71372bccebf8df162904791175313bc83
Gonzalo Chavez <gonzalochavez@uagateway.org>
2016-11-02 16:15:05 +0000

    Finshed Up Lab 3-2

--- unit3/GChavez_numguess.py
+++ unit3/GChavez_numguess.py
@@ -1,5 +1,18 @@
 import random
-for x in range (1):
-    random.randint(1,101)
-
+num = random.randint(1,101)
 guess = int(input("Enter a guess between 1-100: "))
+
+while guess != num:
+    
+    if guess < num:
+        print("Too Low")
+        guess = int(input("Guess again: "))
+        
+    elif guess > num:
+        print("Too High")
+        guess = int(input("Guess again: "))
+        
+if guess == num:
+    print("You got it! It was indeed {}".format(num))
+ 
+
