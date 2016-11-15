eb8b57e1076768364c5b557365f9c2b976dcb891
Gonzalo Chavez <gonzalochavez@uagateway.org>
2016-11-15 17:15:48 +0000

    No Progress On Lab 4-1

--- unit4/GChavez_rps.py
+++ unit4/GChavez_rps.py
@@ -1,2 +1,25 @@
+import random
+
 rounds = input("How nany rounds do you want to play: ")
-option = input("Do you choose rock, paper or scissors:  ")
+p1_choice = input("Do you choose rock, paper or scissors:  ")
+
+def p1_move():
+    if p1_choice == 'rock':
+        return 'r'
+    elif p1_choice == 'p':
+        return 'p'
+    else:
+        return 's'
+
+def comp_move():
+    comp = random.randint("rock", "paper", "scissors")
+    if comp == 'rock':
+        return 'r'
+    elif comp == 'paper':
+        return 'p'
+    else:
+        return 's'
+
+print("P1 chose {}".format(p1_choice))
+print("The computer has chose {}".format(comp_move()))
+    
