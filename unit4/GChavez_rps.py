566a8046ac76a1b034240c3b8741fd634758696b
Gonzalo Chavez <gonzalochavez@uagateway.org>
2016-11-29 17:07:11 +0000

    Finished Lab 4-1

--- unit4/GChavez_rps.py
+++ unit4/GChavez_rps.py
@@ -1,6 +1,5 @@
 import random
 
-
 def get_p1_move():
     p1_move = input("Do you choose rock, paper or scissors:  ")
     if p1_move == "rock":
@@ -21,30 +20,27 @@ def get_comp_move():
         
 def get_rounds():
     rounds = int(input("How many rounds do you want to play from 1 to 9: "))
-    if rounds <= 9:
-        return rounds
-    else:
-        print("Too many rounds")
+    return rounds
 
 def get_round_winner(p1_move, comp_move):
     if p1_move == "r" and comp_move == "p":
-        return "The computer wins"
+        return "computer"
     elif p1_move == "r" and comp_move == "s":
-        return "P1 wins"
+        return "player"
     elif p1_move == "r" and comp_move == "r":
-        return "It's a tie"
+        return "tie"
     elif p1_move == "p" and comp_move == "r":
-        return "P1 wins"
+        return "player"
     elif p1_move == "p" and comp_move == "s":
-        return "The computer wins"
+        return "computer"
     elif p1_move == "p" and comp_move == "p":
-        return "It's a tie"
+        return "tie"
     elif p1_move == "s" and comp_move == "r":
-        return "The computer wins"
+        return "computer"
     elif p1_move == "s" and comp_move == "p":
-        return "P1 wins"
+        return "player"
     elif p1_move == "s" and comp_move == "s":
-        return "It's a tie"
+        return "tie"
 
 def get_full_move(shortmove):
     if shortmove == 'r':
@@ -55,31 +51,42 @@ def get_full_move(shortmove):
         return "scissors"
         
 def print_score(pscore, cscore, ties):
-        return("Player Wins: {} Computer Wins: {} Ties: {}" .format(pscore, cscore, ties))
+        return("Player Wins: {}\nComputer Wins: {}\nTies: {}".format(pscore, cscore, ties))
     
-        
 def rps():
     rounds = get_rounds()
     
-    p1move = get_p1_move()
-    compmove = get_comp_move()
+    pscore = 0
+    cscore = 0
+    ties = 0
     
-    print("Player chose {}".format( get_full_move(p1move)))
-    print("The computer chose {}".format( get_full_move(compmove) ) )
+    for x in range(rounds):
+        p1move = get_p1_move()
+        compmove = get_comp_move()
     
-    winner = get_round_winner(p1move,compmove)
-    if winner == "players":
-        print("Player1 won")
+        print("Player chose {}".format( get_full_move(p1move)))
+        print("The computer chose {}".format( get_full_move(compmove) ) )
+    
+        winner = get_round_winner(p1move,compmove)
+        if winner == "player":
+            print("Player1 won")
+            pscore = pscore + 1
         
-    elif winner == "comp":
-        print("The Computer won")
+        elif winner == "computer":
+            print("The Computer won")
+            cscore = cscore + 1
+            
+        elif winner == "tie":
+            print("It's a tie")
+            ties = ties + 1
+        print(print_score(pscore, cscore, ties))
+        print("-------------------")
         
+    if pscore > cscore:
+        print("Player1 Has Defeated The Computer!")
+    elif cscore > pscore:
+        print("The Computer Has Defeated Player1!")
     else:
-        print("It's a tie")
-    
-#print(get_rounds())
-#print(get_p1_move())
-#print(get_comp_move())
-print(get_round_winner(p1move, cmove))
-#print(get_full_move())
-#print(print_score()
+        print("You Both Tie, So You Both Lose!")
+        
+rps()
