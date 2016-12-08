a3d8d434165fdce8d3f779b95e3d13b96e3adce5
Gonzalo Chavez <gonzalochavez@uagateway.org>
2016-12-08 17:15:32 +0000

    Still working on craps

--- unit4/GChavez_craps.py
+++ unit4/GChavez_craps.py
@@ -16,14 +16,23 @@ def get_bet(bank_amount):
 
         bet = int(input("Bank Acc: 100, How much do you want to bet: "))
         
-def roll2diceO():
+def roll2dice():
     dice1 = random.randint(1,6)
     dice2 = random.randint(1,6)
     dice_sum = dice1 + dice2
     print("Dice 1: {} Dice 2: {}".format(dice1, dice2))
     return dice_sum
 
+def first_roll_result(dice_sum):
+    if dice_sum == 2 or dice_sum == 3 or dice_sum == 12:
+        print("lose")
     
+    elif dice_sum == 7 or dice_sum ==  11:
+        print("win")
+    
+    elif dice_sum != 2 or dice_sum != 3 or dice_sum != 7 or dice_sum != 11 or dice_sum != 12:
+        print(dice_sum)
     
 get_bet(bank_amount)
-roll2diceO()
+roll2dice()
+first_roll_result(roll2dice)
