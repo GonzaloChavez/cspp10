afbaacae82b0f0d180370699c936748db613f42c
Gonzalo Chavez <gonzalochavez@uagateway.org>
2016-12-22 17:16:25 +0000

    On My Last Step In Craps

--- unit4/GChavez_craps.py
+++ unit4/GChavez_craps.py
@@ -21,19 +21,16 @@ def get_roll2dice():
     dice2 = random.randint(1,6)
     dice_sum = dice1 + dice2
     print("Dice 1: {} Dice 2: {}\nDice Sum: {}".format(dice1, dice2, dice_sum))
-    return dice_sum
-
-def get_first_roll(dice_sum):
     if (dice_sum == 2) or (dice_sum == 3) or (dice_sum == 12):
-        print("You Lose!")
         return 'lose'
     
     elif (dice_sum == 7) or (dice_sum ==  11):
-        print("You Won!")
         return 'win'
     
     else:
         return dice_sum
+
+
 
 #function name: update_bank
 #purpose: change bank amount based on whether the player won or not
@@ -43,7 +40,15 @@ def get_first_roll(dice_sum):
 #   result - what happened in the round
 #returns: the updated amount in the bank
 
+def get_update_bank(bet, bank_amount, result):
+    if result == 'lose':
+        bank_amount = bet - bet
+        return("You Lost! Bank Amount: {}".format(bank_amount))
 
+    elif result == 'win':
+        bank_amount = bet + bet
+        return("You Won! Bank Amount: {}".format(bank_amount)) 
+    return bank_amount
         
 def get_second_roll(dice_sum):
     while True:
@@ -59,14 +64,14 @@ def get_second_roll(dice_sum):
         
 def craps():
     bank_amount = 100
-    bet = get_bet(bank_amount)
-    roll2dice = get_roll2dice()
-    dice_sum = roll2dice
-    firstroll = get_first_roll(dice_sum)
-    new_dice_sum = get_second_roll(dice_sum)
 
 
     while bank_amount > 0:
+        bet = get_bet(bank_amount)
+        roll2dice = get_roll2dice()
+        dice_sum = roll2dice
+        new_dice_sum = get_second_roll(dice_sum)
+        
         if dice_sum == 'lose':
             bank_amount = bank_amount - bet
             print("You Lose! Bank Amount: {}".format(bank_amount))
