72db5ddb2147ca419661f65090c778fcf3076a1c
Gonzalo Chavez <gonzalochavez@uagateway.org>
2017-01-12 17:13:08 +0000

    Finished List 1 Assignment

--- unit4/GChavez_craps.py
+++ unit4/GChavez_craps.py
@@ -44,10 +44,13 @@ def get_second_roll(dice_sum):
     while True:
         new_dice1 = random.randint(1,6)
         new_dice2 = random.randint(1,6)
-        new_dice_sum = new_dice1 + new_dice2 
-        if new_dice_sum != 7 or  new_dice_sum != dice_sum:
-                return("Dice 1: {} Dice 2: {}\nDice Sum: {}".format(new_dice1, new_dice2, new_dice_sum))
-        elif  new_dice_sum != dice_sum:
+        new_dice_sum = new_dice1 + new_dice2
+        point_number = new_dice_sum
+        print ("Rolled 2 dice: {} and {}, {} in total.".format(new_dice1, new_dice2, new_dice_sum))
+        if point_number == dice_sum:
+            return "win"
+        elif point_number == 7:
+            return "lose"
             
         
 def craps():
@@ -55,9 +58,8 @@ def craps():
 
     while bank_amount > 0:
         bet = get_bet(bank_amount)
-        roll2dice = get_roll2dice()
-        dice_sum = roll2dice
-        new_dice_sum = get_second_roll(dice_sum)
+        dice_sum = get_roll2dice()
+
         
         if dice_sum == 'lose':
             bank_amount = bank_amount - bet
@@ -65,12 +67,17 @@ def craps():
         elif dice_sum == 'win':
             bank_amount = bank_amount + bet
             print("You Win! Bank Amount: {}".format(bank_amount))
-        elif dice_sum == dice_sum:
-            print("Your Point Nunber: {}".format(dice_sum))
+        else:
+            point_number = get_second_roll(dice_sum)
 
-            if new_dice_sum == dice_sum:
+            if point_number == 'win':
                 bank_amount = bank_amount + bet
                 print("You Win! Bank Amount: {}".format(bank_amount))
+            
+            elif point_number == 'lose':
+                bank_amount = bank_amount - bet
+                print("You Lose! Bank Amount: {}".format(bank_amount))
+       
         
             
     
--- unit4/overunder7.py
+++ unit4/overunder7.py
@@ -28,7 +28,7 @@ def get_bet(bank):
     #   available in bank, then get new bet
     while True:
         if bet < 0:
-            print("No negative numbers")
+            print("No Negative Numbers")
             
         elif bet > bank:
             print("You Don't Have That Kind Of Money!")
@@ -62,7 +62,7 @@ def get_range(dice_sum):
         
 # function for getting the user's choice of range
 # name: choose_range
-# arguments: none
+# arguments: none 
 # returns: player's choice of range
 #       "over7", "under7", or "equal7"
 def choose_range():
@@ -74,6 +74,41 @@ def choose_range():
     
 # function for the main game
 def overunder7():
-    bet = get_bet(bank)
+    bank = 100
+    #loop as long as the player has SOME amount of money
+    while bank > 0:
+        #ask for a bet and save it
+        bet = get_bet(bank)
+        #ask for the range and save it
+        user_range = choose_range()
+        #roll 2 dice and save it
+        dice = roll2dice()
+        #figure out the range of the dice and save it
+        round_range = get_range(dice)
+        #check to see if the user won or lost
+        #update their bank accordingly
+        if user_range == round_range: #user won
+            print("You win!")
+            if user_range == "equal7":
+                bank = bank + 4 * bet
+            else:
+                bank = bank + bet
+            
+        else: #user lost
+            print("You lost!")
+            bank = bank - bet
+            
+        #print new bank value
+        print("Your balance is ${}".format(bank))
+        
+        #ask if they want to do another round
+        new_round = input("Do you want to continue? [y|n]").lower()
+        if new_round != "y":
+            break
+    
+    print("Game over, you have ${} in your bank".format(bank))
+    
+    
+overunder7()
     
 overunder7()
