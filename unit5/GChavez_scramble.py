7a83e4980b11fc157ee2a9cd71be4bf9faa35574
Gonzalo Chavez <gonzalochavez@uagateway.org>
2017-02-03 17:14:47 +0000

    Still Working on the functions

--- unit5/GChavez_scramble.py
+++ unit5/GChavez_scramble.py
@@ -1,13 +1,14 @@
 import random
-#function to scramble a single word
+#function to scramble a single word where the first letter always stay the same
 #name: scramble_word
 #arguments: none
 #returns: the scramble word 
 def scramble_word():
     user_input = input("Enter a word: ")
     some_list = list(user_input)
-    random.shuffle(some_list)
-    print(some_list)
+    while some_list[0]:
+        random.shuffle(some_list)
+        print(some_list)
 #function to scramble a series of words
 #name: scramble_phase
 #arguments: scramble_word
