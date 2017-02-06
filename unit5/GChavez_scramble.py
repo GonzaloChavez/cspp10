77a979e49add65ec634ab4cdd7fe13e43203dc57
Gonzalo Chavez <gonzalochavez@uagateway.org>
2017-02-06 17:16:06 +0000

    Working on the index

--- unit5/GChavez_scramble.py
+++ unit5/GChavez_scramble.py
@@ -1,18 +1,20 @@
 import random
+
 #function to scramble a single word where the first letter always stay the same
 #name: scramble_word
 #arguments: none
 #returns: the scramble word 
 def scramble_word():
-    user_input = input("Enter a word: ")
-    some_list = list(user_input)
-    while some_list[0]:
-        random.shuffle(some_list)
-        print(some_list)
+    word_1 = input("Enter a word: ")
+    num = 0
+    some_list = list(word_1)
+    random.shuffle(some_list)
+    print(some_list)
 #function to scramble a series of words
 #name: scramble_phase
 #arguments: scramble_word
 #returns: a series of words that are scramble
-#def scramble_phase():
+def scramble_phase(scramble_word):
+    user_input2 = input("Enter a sentence ")
 
 scramble_word()
