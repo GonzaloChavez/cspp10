b366471d167778bce8420fffda9529121bdc457a
Gonzalo Chavez <gonzalochavez@uagateway.org>
2017-02-16 17:15:36 +0000

    Working on the main function

--- unit5/dictionary_add.py
+++ unit5/dictionary_add.py
@@ -2,35 +2,43 @@ from pprint import pprint
 
 d = {
 }
-
-def user_choice():
-    user_input = input("Add? Remove Key? Update? Print? Exit? ")
+user_input = input("Add? Remove Key? Update? Print? Exit? ")
     
-def Add(user_choice):
-    if user_input == "Add":
-        key = input("Enter A Key ")
-        value = input("Enter A Value ")
-        d[key] = value
-        pprint(d)
+def Add(user_input):
+        if user_input == "Add":
+            key = input("Enter A Key ")
+            value = input("Enter A Value ")
+        for key in d:
+            if d[key] == value:
+                print("Already exists!")
+            else:
+                d[key] = value
 
-def RemoveKey(user_choice):
+def RemoveKey(user_input):
     if user_input == "Remove Key":
-        del d[input]
-        pprint(d)
+        delete = input("What Key Do You Want To Delete? ")
+        del d[delete]
     
-def Update(user_choice):
+def Update(user_input):
     if user_input == "Update":
-        d[input] = input
-        pprint(d)
+        update = input("What Key Do You Want To Update? ")
+        new_update = input("What Do You Want It To Update To? ")
+        d[update] = new_update
 
-def Print(user_choice):
-    if input == "Print":
+def Print(user_input):
+    if user_input == "Print":
         print(d)
-
-def Exit(user_choice):
-    if input == "Exit":
+def Exit(user_input):
+    if user_input == "Exit":
         print("Done")
-        break
+ 
 
 def dictionary():
-    
+    while True:
+        Add(user_input)
+        RemoveKey(user_input)
+        Update(user_input)
+        Print(user_input)
+        Exit(user_input)
+        
+dictionary()
