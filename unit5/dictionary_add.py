40067d4e93a0801e01f42d097034caf8c329220e
Gonzalo Chavez <gonzalochavez@uagateway.org>
2017-02-17 17:15:14 +0000

    Need Help!

--- unit5/dictionary_add.py
+++ unit5/dictionary_add.py
@@ -8,11 +8,8 @@ def Add(user_input):
         if user_input == "Add":
             key = input("Enter A Key ")
             value = input("Enter A Value ")
-        for key in d:
-            if d[key] == value:
-                print("Already exists!")
-            else:
-                d[key] = value
+            d[key] = value
+            print(d)
 
 def RemoveKey(user_input):
     if user_input == "Remove Key":
@@ -27,18 +24,18 @@ def Update(user_input):
 
 def Print(user_input):
     if user_input == "Print":
-        print(d)
+        pprint(d)
 def Exit(user_input):
     if user_input == "Exit":
         print("Done")
  
 
 def dictionary():
-    while True:
+    while user_input != "Exit":
         Add(user_input)
         RemoveKey(user_input)
         Update(user_input)
         Print(user_input)
-        Exit(user_input)
+    Exit(user_input)
         
 dictionary()
